a64b0d02144bdb6916dda9f869e5b2b1b3d94a3a
BychkovDmitry <dmitry.bychkov@lunalabs.io>
2022-02-24 12:22:52 +0300

    Remove stupid test

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,24 +2,15 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    private GenMeshCircle _genMeshCircle;
-    
-    private void Awake()
+    private void Start()
     {
-        float startTime = Time.realtimeSinceStartup;
-        _genMeshCircle = new GenMeshCircle();
-
-        for (int i = 0; i < 10; i++)
-        {
-            GenerateCircle();
-        }
-        
-        Debug.Log((Time.realtimeSinceStartup - startTime) * 1000f + "ms");
+        GenerateCircle();
     }
     
     private void GenerateCircle()
     {
-        GameObject circle = _genMeshCircle.Construct(
+        GenMeshCircle genMeshCircle = new();
+        GameObject circle = genMeshCircle.Construct(
             Random.Range(0.01f, 1), 
             Random.Range(0.01f, 1), 
             (byte)Random.Range(3,90),
